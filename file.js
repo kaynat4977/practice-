dd30d8d2de4f8d6f3e66c5b2eedda9c55fe18daf
Kaynat Shabbir <shabbirkainat75@gmail.com>
2024-11-14 03:41:54 -0800

    Add files via upload

---
+++ file.js
@@ -0,0 +1,160 @@
+//QUESTION NO1//
+
+//let city =prompt("Enter your city name");
+//if(city == "karachi"){
+  //  alert("Welcome to the city of ligth");
+//}
+//else{
+   // alert("welcome to the city of karachi");
+//}
+
+//QUESTION NO2//
+//let gender =prompt("whats youre gender");
+//if(gender == "female"){
+  // alert("good morning Maam");
+//}
+//else if 
+  //(gender == "male"){
+   // alert("good morning sir");
+  //}
+  //else{
+   // document.write("gender is invalid");
+  //}
+
+  //QUESTION NO3//
+  //let color =prompt("enter traffic color lights");
+  //if(color == "red"){
+  //  alert("must stop plz !!")
+   // }
+    //else if (color =="yellow"){
+    //  alert("redy to go");
+    //}
+    //else if(color =="green"){
+     // alert("Go Now!!");
+    //}
+
+    //QUESTION NO 4//
+    //let fuel=("how much fuel is in your car?");
+    //if(fuel<0.25+"literes"){
+    //  alert("plz refill fuel in youre car");
+   // }
+    //else{
+     // alert("fuel is suffficeint");
+    //}
+
+    //QUESTION NO 6//
+    //let sub1 =parseInt(prompt("Enter your english marks"));
+    //let sub2 =parseInt(prompt("Enter your maths marks"));
+   // let sub3 =parseInt(prompt("Enter your physics marks"));
+   // let totalmarks =prompt("enter total marks");
+
+  //let totalmarksobtained=sub1+sub2+sub3;
+  //document.write("RESULT +,br>");
+ // document.write("total marks :, totalmarks"+ "<br>");
+ // document.write("marks obtained:, totalmarksobtained" + "<br>");
+  //let precentage =(totalmarksobtained/ttotalmarks*100);
+  //document.write("percentage:", precentage+"%"+"<br>");
+
+ // if (totalmarksobtained>=80){
+  //  document.write("Grade: A-one"+"<br>");
+  //  document.write("REmarks: super excellent");
+  //}
+  //else if
+  //  (totalmarksobtained>=70){
+//document.write("Grade :A +");
+//document.write("Remarks :Good");
+   // }
+   // else if
+    //(totalmarksobtained>=60){
+     // document.write("Grade :B");
+     // document.write("Remarks : Improve youre result");
+   // }
+//else{
+ // document.write("Grade : fail");
+ // document.write("REAMRKS: sorry you are fail");
+//}
+
+//QUESYION 7//
+//const secretnumber=8;
+//const guess=parseInt(prompt("guess secrete number (btw 1 and 10):"));
+
+//if(guess ===secretnumber){
+ // alert("BINGO !! right answer");
+//}
+//else if (guess +1=== secretnumber){
+  //alert("close enough to the correct answer");
+//}
+//else{
+  //alert("guess wrong plz try again");
+//}
+
+//QUESTION 8//
+//let number=prompt("enter a number");
+//if(number%2!==0){
+  //alert("Number can be divisible by 3");
+//}
+
+//QUESTION NO 9//
+//let num =prompt("enter a number");
+//if(num%2==0){
+  //alert("even number");
+//}
+//if(num%2!==0){
+  //alert("odd number");
+//}
+
+//QUESTION NO 10//
+//let temperature =prompt("current temperature of youre area");
+//if(temperature>40){
+  //document.write("too hot temp outside");
+//}
+//else if(temperature>30){
+  //document.write("Noraml weather today");
+//}
+//else if(temperature>20){
+  //document.write("windy weather today");
+//}
+ // else if(temperature>10){
+   // document.write("oppss !! too cool weather today");
+  //}
+
+  //QUESTION NO 11//
+  //let num1 =parseInt(prompt("enter first number"));
+  //let num2 =parseInt(prompt("enter second number"));
+  //let operations =prompt("enter operations :\n + \n - \n * \n % \n /");
+
+  //if (operations === "+"){
+   // document.write(num1+num2);
+  ///}
+  //else if(operations === "-"){
+  //  document.write(num1-num2);
+  //}
+
+  //else if(operations === "*"){
+   // document.write(num1*num2);
+  //}
+
+ // else if(operations === "/"){
+  //  document.write(num1/num2);
+  //}
+//else if(operations ==="%"){
+ // document.write(operations=="%");
+//}
+
+
+
+
+
+
+
+
+
+
+  
+  
+
+  
+
+
+
+  
